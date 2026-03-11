f4f62947a6c8e66ef50703f9a840b6aa57eefbc3
valentinalopezgrc <lavaloga17@gmail.com>
2026-03-10 22:24:40 -0500

    fix: placeholder equipos/registrar para evitar error de compilación

--- src/app/pages/equipos/registrar/registrar.ts
+++ src/app/pages/equipos/registrar/registrar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registrar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './registrar.html',
-  styleUrl: './registrar.scss',
+  styleUrl: './registrar.scss'
 })
 export class Registrar {
-
 }
