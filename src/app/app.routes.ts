ea689403d61e0498a687d6279dcd8e00f25ade20
valentinalopezgrc <lavaloga17@gmail.com>
2026-03-11 23:11:04 -0500

    fix:styles.scss combinado con estilos de Alejandro y Valentina y app.routes.ts unificado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,27 +9,15 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { RegistroUsuario } from './pages/registro-usuario/registro-usuario';
 import { RegistrarPlaneta } from './pages/planetas/registrar/registrar';
+import { ListarComponent } from './pages/planetas/listar/listar';
+import { RegistrarEquipoComponent } from './pages/equipos/registrar/registrar';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'app/planetas/listar',
-    pathMatch: 'full'
-  },
-  {
-    path: 'app',
-    children: [
-      {
-        path: 'planetas/listar',
-        loadComponent: () =>
-          import('./pages/planetas/listar/listar').then(m => m.ListarComponent)
-      },
-      {
-        path: 'equipos/registrar',
-        loadComponent: () =>
-          import('./pages/equipos/registrar/registrar').then(m => m.RegistrarEquipoComponent)
-      },
-      { path: '**', redirectTo: 'app/planetas/listar' }
-    ]
-  }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'registrar-usuario', component: RegistroUsuario },
+  { path: 'planetas/registrar', component: RegistrarPlaneta },
+  { path: 'planetas', component: ListarComponent },
+  { path: 'equipos/registrar', component: RegistrarEquipoComponent },
+  { path: '**', redirectTo: 'login' }
 ];
